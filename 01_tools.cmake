78ed6b729140d8c60e46a2c876258375206e71b7
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2022-02-24 11:48:29 -0300

    added recursive submodule on git clone

--- 01_tools.cmake
+++ 01_tools.cmake
@@ -312,7 +312,7 @@ macro(tool_download_git_package REPOSITORY_URL LIBNAME)
         find_package(Git REQUIRED)
 
         execute_process(
-            COMMAND "${GIT_EXECUTABLE}" clone "${REPOSITORY_URL}" "${ARIBEIRO_LIBS_DIR}/${LIBNAME}/"
+            COMMAND "${GIT_EXECUTABLE}" clone --recurse-submodules "${REPOSITORY_URL}" "${ARIBEIRO_LIBS_DIR}/${LIBNAME}/"
             OUTPUT_VARIABLE result
             #COMMAND_ERROR_IS_FATAL ANY
         )
